abc8a862da913f1014896dd34a364739e55a1755
harisaini778 <106510031+harisaini778@users.noreply.github.com>
2023-02-05 02:19:50 +0530

    Add files via upload
    appointment booking app

---
+++ dom3.js
@@ -0,0 +1,43 @@
+// var form = document.getElementById("main-form");
+// form.addEventListener("submit",runEvent);
+// console.log(1);
+// function runEvent(e1){
+//     e1.preventDefault();
+//     console.log(1);
+    // var formControl = document.getElementById("main-form");
+    // console.log(formControl);
+    // formControl.addEventListener("keyup",copyText);
+    // function copyText(e2) {
+    //     e2.preventDefault();
+    //     var text1 = document.getElementById("item1").value;
+    //     var text2 = document.getElementById("item2").value;
+    //     var result = text1.concat(" "+text2);
+    //     var formSubmit = document.getElementById("main-form");
+    //     formSubmit.addEventListener("submit",saveDetails);
+    //     function saveDetails(e3){
+    //         e3.preventDefault();
+    //         console.log(1);
+    //         console.log(e3.target.value);
+    //     }
+    //     console.log(result);
+    // }
+//     var val1 = document.getElementById("item1").value;
+//     var val2 = document.getElementById("item2").value;
+//     console.log(val1);
+//     console.log(val2);
+//     console.log(2);
+//     localStorage.setItem(val1,val2);
+// }
+var form = document.getElementById("main-form");
+form.addEventListener("submit",copyText);
+function copyText(e1){
+    e1.preventDefault();
+    var val1 = document.getElementById("item1").value;
+    var val2 = document.getElementById("item2").value;
+    localStorage.setItem(val1,val2);
+    var result = val1.concat(" "+val2);
+    var result1 = result.toLowerCase();
+    console.log(result1);
+    document.getElementById("item1").value = " ";
+    document.getElementById("item2").value = " ";
+}
