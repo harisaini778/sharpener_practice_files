37bf80733bb5d5dd38941dc93fa3148dfe4e16f6
harisaini778 <106510031+harisaini778@users.noreply.github.com>
2023-02-05 22:52:27 +0530

    Add files via upload
    dom task-12
    In this example, the saveData function takes in two parameters: key and value. The key is used to identify the data in the local storage and the value is the data you want to store. The function first retrieves any existing data stored under the specified key and parses it into a JavaScript object. Then, it pushes the new value into the data array. Finally, it stringifies the updated data array and saves it back to the local storage using localStorage.setItem method.

---
+++ dom5.js
@@ -0,0 +1,73 @@
+// var form = document.getElementById("main-form");
+// form.addEventListener("submit",runEvent);
+// console.log(1);
+// function runEvent(e1){
+//     e1.preventDefault();
+//     console.log(1);
+    // var formControl = document.getElementById("main-form");
+    // console.log(formControl);
+    // formControl.addEventListener("keyup",copyText);
+    // function copyText(e2) {
+    //     e2.preventDefault();
+    //     var text1 = document.getElementById("item1").value;
+    //     var text2 = document.getElementById("item2").value;
+    //     var result = text1.concat(" "+text2);
+    //     var formSubmit = document.getElementById("main-form");
+    //     formSubmit.addEventListener("submit",saveDetails);
+    //     function saveDetails(e3){
+    //         e3.preventDefault();
+    //         console.log(1);
+    //         console.log(e3.target.value);
+    //     }
+    //     console.log(result);
+    // }
+//     var val1 = document.getElementById("item1").value;
+//     var val2 = document.getElementById("item2").value;
+//     console.log(val1);
+//     console.log(val2);
+//     console.log(2);
+//     localStorage.setItem(val1,val2);
+// }
+var form = document.getElementById("main-form");
+form.addEventListener("submit",copyText);
+function copyText(e1){
+    e1.preventDefault();
+    var val1 = document.getElementById("item1").value;
+    var val2 = document.getElementById("item2").value;
+    //localStorage.setItem(val1,val2);
+    let myObj = {
+    fullName:val1,
+    validEmail:val2
+};
+    var result = val1.concat(" "+val2);
+    var result1 = result.toLowerCase();
+    //console.log(result1);
+    //localStorage.setItem("obj",myObj);
+    let myObjSerialised = JSON.stringify(myObj);
+    localStorage.setItem("obj",myObjSerialised);
+    //console.log(myObjSerialised);
+    let myObjDeserialised = JSON.parse(localStorage.getItem("obj"));
+    console.log(myObjDeserialised);
+    var addUserList = document.getElementById("userList");
+    var addUserDetails = document.createElement("li");
+    addUserDetails.className="list-group-item";
+    var textItem = document.createTextNode(`${val1} : ${val2}`);
+    addUserDetails.appendChild(textItem);
+    addUserList.appendChild(addUserDetails);
+    function saveData(key,val) {
+    let data;
+    if(localStorage.getItem(key)) {
+        data = JSON.parse(localStorage.getItem(key));
+        console.log(data);
+    }
+    else {
+        data = [];
+    }
+    data.push(val);
+    localStorage.setItem(val1,JSON.stringify(data));
+    }
+    saveData(val1,val2);
+    document.getElementById("item1").value = " ";
+    document.getElementById("item2").value = " ";
+}
+
